ec0aa2ef2d857997cf784960c4519565760a9a24
Dinesh Panicker <dinesh.panicker2000@gmail.com>
2023-08-21 00:00:18 +0530

    Web Dev Learnings

---
+++ Home/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-secondary mt-5">
+        Submit
+      </button>
+      <div>
+        <button type="button" className="btn btn-primary mt-5">
+          Manage Disabled
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
